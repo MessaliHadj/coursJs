8d38f97f8dab9c5d4e9a734f4cbc7596e95136bb
Messali <lanymale33@gmail.com>
2023-02-14 14:18:38 +0100

    img

---
+++ README.md
@@ -0,0 +1,16 @@
+# Cours de JavaScript  
+## Introduction au JavaScript 
+
+
+```
+var a = 42;
+var b = 4.2;
+var c = -42;
+var d = "42";
+
+alert("ma variable a est de type : " + typeof(a) +
+"\nma variable b est de type : " + typeof(b) +
+"\nma variable c est de type : " + typeof(c) +
+"\nma variable d est de type : " + typeof(d));
+```
+
--- main.js
+++ main.js
@@ -7,14 +7,37 @@
 // monTableau["maman"]= "tati";
 // monTableau["papa"]= "tutu";
 
-console.table(monTableau);
-const rond = document.getElementById('rond');
-const carre = document.getElementsByClassName('carre')
-console.log(carre);
+// console.table(monTableau);
+// const rond = document.getElementById('rond');
+// const carre = document.getElementsByClassName('carre')
+// console.log(carre);
 
-function maFonction(long, larg) {
-  console.log("la surface est de " + long*larg + " m²");
-}
-maFonction(3, 5)
-maFonction(33, 5)
-maFonction(3, 50)
+// function maFonction(long, larg) {
+//   console.log("la surface est de " + long*larg + " m²");
+// }
+// maFonction(3, 5)
+// maFonction(33, 5)
+// maFonction(3, 50)
+var a = 42;
+var b = 4.2;
+var c = -42;
+var d = "42";
+
+alert("ma variable a est de type : " + typeof(a) +
+"\nma variable b est de type : " + typeof(b) +
+"\nma variable c est de type : " + typeof(c) +
+"\nma variable d est de type : " + typeof(d));
+
+var machaine = "exemple";
+var maChaine = new String("exemple");
+
+alert("ma variable machaine est de type : " + typeof(machaine) +
+"\nma variable maChaine est de type : " + typeof(maChaine));
+
+var n = null;
+var u = undefined;
+var nn = NaN;
+
+alert("ma variable n est de type : " + typeof(n) +
+"\nma variable u est de type : " + typeof(u) +
+"\nma variable nn est de type : " + typeof(nn));
