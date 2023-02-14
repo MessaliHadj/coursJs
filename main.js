91cbd54ffe207f6ac34f79b63bec16e8593db391
Messali <lanymale33@gmail.com>
2023-02-14 10:57:11 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// const firstname = "Sofiane";
+// const lastname = "Khalfi";
+// var age = 26;
+// // var monTableau = new Array;
+// var monTableau = [];
+// monTableau["enfant"]= "toto";
+// monTableau["maman"]= "tati";
+// monTableau["papa"]= "tutu";
+
+// console.table(monTableau);
+const rond = document.getElementById('rond');
+const carre = document.getElementsByClassName('carre')
+console.log(carre);
+
+function maFonction(long, larg) {
+  console.log("la surface est de " + long*larg + " m²");
+}
+maFonction(3, 5)
+maFonction(33, 5)
+maFonction(3, 50)
